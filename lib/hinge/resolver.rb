bee2f24864c9eee60bd45342fd099102f78f5cb5
Thomas Stratmann <thomas.stratmann@9elements.com>
2017-03-07 23:56:48 +0100

    naive implementation

--- lib/hinge/resolver.rb
+++ lib/hinge/resolver.rb
@@ -1,9 +1,86 @@
-require 'hinge/resolver'
-
 module Hinge
   class Resolver
+    Error = Class.new(RuntimeError)
+    UnknownDependency = Class.new(Error)
+    CircularReference = Class.new(Error)
+
     def initialize(container)
       @container = container
+      @nodes = {}
+      @resolved = {}
+    end
+    attr_reader :container
+    attr_reader :resolved
+
+    def resolve(name)
+      return resolved[name] if resolved.key?(name)
+
+      raise ArgumentError unless name.is_a?(Symbol)
+
+      node = node(name)
+      populate_node(node)
+
+      resolved[name]
+    end
+
+    private
+
+    def populate_node(node, history = [])
+      raise UnknownDependency unless node.method # TODO nice message
+      raise CircularReference if history.include?(node.name)
+
+      node.dependency_names.each do |dep_name|
+        populate_node node(dep_name), history + [node.name]
+      end
+
+      ordered_dependent_values =
+        node.dependency_names.map do |dep_name|
+          @resolved[dep_name]
+        end
+      @resolved[node.name] = node.invoke(ordered_dependent_values)
+    end
+
+    def node(name)
+      @nodes[name] ||= Node.new(name, container)
+    end
+
+    class Node
+      def initialize(name, container)
+        @name = name
+        @method_name = "build_#{@name}"
+        @method =
+          begin
+            container.method(@method_name)
+          rescue NameError
+            nil
+          end
+
+        if @method
+          @dependency_names = []
+          @positional_args_count = 0
+          @keyword_arg_names = []
+
+          @method.parameters.each do |kind, argument_name|
+            argument_name = argument_name.to_sym
+            @dependency_names << argument_name
+            case kind
+            when :req
+              @positional_args_count += 1
+            when :keyreq
+              @keyword_arg_names << argument_name
+            end
+          end
+        end
+      end
+      attr_reader :name, :method_name, :method, :dependency_names
+
+      def invoke(ordered_values)
+        splat = ordered_values.take(@positional_args_count)
+        splat << Hash[
+          @keyword_arg_names.zip(ordered_values.drop(@positional_args_count))
+        ] if @keyword_arg_names.any?
+        @method.call(*splat)
+      end
     end
   end
 end
---
+++ spec/hinge/resolver_spec.rb
@@ -0,0 +1,152 @@
+require 'hinge/resolver'
+
+describe Hinge::Resolver do
+  it "has the container attribute" do
+    container = "foo"
+    expect(described_class.new(container).container).to be_equal(container)
+  end
+
+  describe "initially" do
+    it "has nothing resolved" do
+      container = "foo"
+      expect(described_class.new(container).resolved).to eql({})
+    end
+  end
+
+  describe "with some container" do
+    subject do
+      described_class.new(container)
+    end
+
+    describe "the second simplest case" do
+      let(:container) do
+        container = Object.new
+        def container.build_simple
+          "simple"
+        end
+        container
+      end
+
+      describe "resolve" do
+        it "does not like string names" do
+          # I like to keep things simple. Use symbols instead!
+          expect {
+            subject.resolve("simple")
+          }.to raise_exception(ArgumentError)
+        end
+
+        it "resolves a leaf value correctly" do
+          expect(
+            subject.resolve(:simple)
+          ).to eql(container.build_simple)
+
+          expect(
+            subject.resolved[:simple]
+          ).to eql(container.build_simple)
+        end
+
+        it "produces an equal value the second time" do
+          expect(
+            subject.resolve(:simple)
+          ).to equal(subject.resolve(:simple))
+        end
+
+        it "raises when the container does not have a prefixed method" do
+          expect {
+            subject.resolve(:similar)
+          }.to raise_exception(described_class::UnknownDependency)
+        end
+      end
+    end
+
+    describe "with a simple dependency" do
+      let(:container) do
+        container = Object.new
+        def container.build_root(leaf)
+          "root " + leaf
+        end
+        def container.build_leaf
+          "leaf"
+        end
+        container
+      end
+
+      it "resolves correctly" do
+        expect(
+          subject.resolve(:root)
+        ).to eql("root leaf")
+      end
+    end
+
+    describe "with a missing dependency" do
+      let(:container) do
+        container = Object.new
+        def container.build_root(leaf)
+        end
+        container
+      end
+
+      it "detects incompleteness" do
+        expect {
+          subject.resolve(:root)
+        }.to raise_exception(described_class::UnknownDependency)
+      end
+    end
+
+    describe "with a self-referential method" do
+      let(:container) do
+        container = Object.new
+        def container.build_root(root)
+        end
+        container
+      end
+
+      it "detects loop" do
+        expect {
+          subject.resolve(:root)
+        }.to raise_exception(described_class::CircularReference)
+      end
+    end
+
+    describe "with a loop spanning more than one dependency" do
+      let(:container) do
+        container = Object.new
+        def container.build_leaf(root)
+        end
+        def container.build_root(leaf)
+        end
+        container
+      end
+
+      it "detects loop" do
+        expect {
+          subject.resolve(:root)
+        }.to raise_exception(described_class::CircularReference)
+      end
+    end
+
+    describe "possible argument names" do
+      let(:container) do
+        container = Object.new
+        def container.build_positional
+          :positional
+        end
+        def container.build_named
+          :named
+        end
+        def container.build_root(positional, named)
+          [positional, named]
+        end
+        container
+      end
+
+      it "resolves both positional and named arguments" do
+        expect(
+          subject.resolve(:root)
+        ).to eql([:positional, :named])
+      end
+    end
+  end
+
+end
+
